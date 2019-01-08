c6e331b94425d02dff59d814d5424dcf1d79290a
Vladimir Rodríguez <fizzvr@gmail.com>
2019-01-07 23:22:59 -0500

    Npm run build files. > Developer 📲
    [Fix: ]
    ---
    [Desarrollo Web NodeJS]

---
+++ build/precache-manifest.910159acc3dfc3bae3cd01d509afe254.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6a6eec46ff7547cf7872",
+    "url": "/static/js/main.6a6eec46.chunk.js"
+  },
+  {
+    "revision": "deba3206ce6fc32f7b60",
+    "url": "/static/js/1.deba3206.chunk.js"
+  },
+  {
+    "revision": "6a6eec46ff7547cf7872",
+    "url": "/static/css/main.a8fa9661.chunk.css"
+  },
+  {
+    "revision": "271c02c8a7c25adb3b2c88b6c75af86f",
+    "url": "/index.html"
+  }
+];
